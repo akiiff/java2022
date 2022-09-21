0f1e9c5eb172b9d5c679c912829c66b5fb103753
ako <ako@ako-pc.home>
2022-09-21 20:20:42 +0300

    ee

---
+++ kalınSesliİnceSesliHarfler/src/kalınSesliİnceSesliHarfler/Main.java
@@ -0,0 +1,24 @@
+package kalınSesliİnceSesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//kalın sesli ve ince sesli harfler
+		char harf = 'ı';
+		switch (harf) {
+		case 'A':
+		case 'O':
+		case 'I':
+		case 'U':
+			System.out.println("kalın sesli harftir.");
+			break;
+			default :
+			System.out.println("ince sesli harftir.");
+			
+		}
+		
+		
+
+	}
+
+}
